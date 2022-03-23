d2a3db2a1637a301be3405d94f4b2cc585c6e674
sebastianjinich <sjinich@estudiantes.unsam.edu.ar>
2022-03-22 23:50:01 -0300

    costo camion update

---
+++ Ejercicios/Clase03/costo_camion.py
@@ -0,0 +1,58 @@
+import csv
+
+def costo_camion(nombre_archivo):
+    costo_total = 0
+    with open(nombre_archivo,'rt') as archivo:
+        archivo_csv = csv.reader(archivo)
+        headers = next(archivo_csv)
+        for nro_fila, fila in enumerate(archivo_csv, start=1):
+            registro = dict(zip(headers, fila))
+            print(registro)
+            try:
+                costo_total += int(registro['cajones'])+float(registro['precio'])
+            except ValueError:
+                print(f'Fila {nro_fila}: No es interpretable: {fila}')
+    print('Costo del camion: ', costo_total)
+    return costo_total
+
+
+def leer_camion(nombre_archivo:str)->dict:
+	camion = list()
+	with open(nombre_archivo,'rt') as archivo:
+		archivo_csv = csv.reader(archivo)
+		headers = next(archivo_csv)
+		for renglon in archivo_csv:
+			# Es necesario que el renglon tenga contenido.
+			if len(renglon) > 0:
+				camion.append(dict(zip(headers,renglon)))
+
+	return camion	 
+
+def leer_precios(nombre_archivo:str)->dict:
+    # Precios no tiene encabezados, mantengo el mismo formato que en informe.py Porque va a ser hardcodeado de cualquier manera.
+	precios = dict()
+	with open(nombre_archivo,'rt') as archivo:
+		archivo_csv = csv.reader(archivo)
+		for renglon in archivo_csv:
+			if len(renglon) == 2:
+				precios[renglon[0]] = float(renglon[1])
+			# No me convence usar Try - Except puede traer bugs en el codigo, como por ejemplo
+			# indexar un renglon erroneo, que tenga 3 entrdas. Con if tiene mas sentido y es menos probable que
+			# aparezcan bugs. Tambien se podria validar los datos antes de introducirlos en el diccionario.
+
+	return precios	 
+
+precios = leer_precios('../Data/precios.csv')
+camion = leer_camion('../Data/fecha_camion.csv')
+
+costo_camion_total= 0
+venta = 0
+
+for fruta in camion:
+	cajones = int(fruta.get('cajones'))
+	precio_cajon = float(fruta.get('precio'))
+	venta_cajon = precios.get(fruta.get('nombre')) 
+	costo_camion_total += cajones * precio_cajon
+	venta += cajones * venta_cajon
+
+print(f'Costo del camion: {round(costo_camion_total,2)}\nVentas recaudadas: {round(venta,2)}\nBalance: {round(venta - costo_camion_total,2)}')
